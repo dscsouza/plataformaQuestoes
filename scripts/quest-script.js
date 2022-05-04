f545643b27a1c6e7f3f7a5bbe21b70c9939f0f31
dscsouza <dscsouza@gmail.com>
2022-05-04 19:15:56 -0400

    limite de 5 questoes por consulta

--- scripts/quest-script.js
+++ scripts/quest-script.js
@@ -66,7 +66,7 @@ if (escuro == "true"){
 function allIdQuestion(){
     
     
-        db.collection(questoes).get().then(snapshot=>{
+        db.collection(questoes).limit(5).get().then(snapshot=>{
             snapshot.forEach((docs) =>{
                idQuestions.push(docs.id);
                questions.push(docs.data())
