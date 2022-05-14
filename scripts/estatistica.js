8ec29c4724b411dbd939625398ab5206745e55e2
Deyvison <dscsouza@gmail.com>
2022-05-14 02:31:07 -0400

    estatistica inicial implementada - fazer gráficos etc...

--- scripts/estatistica.js
+++ scripts/estatistica.js
@@ -1,15 +1,18 @@
 estatistica = []
 let qtdCerta = 0
-let qtdErrada = 0 
+let qtdErrada = 0
+let msg = ' '
 
-function resgatarDadosEstatistica(){
+function resgatarDadosEstatistica(tag){
     estatistica = []
+    qtdCerta = 0
+    qtdErrada = 0
     idUser = firebase.auth().currentUser.uid
 
 
     //consulta os comentários no banco de dados
     //eles ficam salvos em uma collection dentro da questão
-    db.collection(usuarios).doc(idUser.toString())
+    func1 = db.collection(usuarios).doc(idUser.toString())
     .collection('questoes').get().then(snapshot=>{
         snapshot.forEach(doc=>{
             estatistica.push(doc.data())
@@ -21,8 +24,21 @@ function resgatarDadosEstatistica(){
         totalResolvidas = estatistica.length
         console.log(`Você respondeu ${totalResolvidas} questões. Acertou ${qtdCerta}, errou ${qtdErrada}, \n ${((qtdCerta/totalResolvidas)*100).toFixed(2)}% de acertos e ${((qtdErrada/totalResolvidas) * 100).toFixed(2)}% de erro.`)
         
+        
+    })
+    
+    func1.then(()=>{
+
+        msg = `Você respondeu ${totalResolvidas} questões. Acertou ${qtdCerta}, errou ${qtdErrada}. Ou seja, está com \n ${((qtdCerta/totalResolvidas)*100).toFixed(2)}% de acertos e ${((qtdErrada/totalResolvidas) * 100).toFixed(2)}% de erro.`
+
+        exibirEstatiscas()
     })
 
-    
-    
+}
+
+function exibirEstatiscas(){
+  
+    document.querySelector("#resultadoEstatistica").innerHTML = msg
+   
+
 }
